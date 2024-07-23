1e515903cf15bcc50f8056562cbab48399becdfd
Alex Kalkatos <alexcsts@gmail.com>
2024-07-23 18:36:44 -0300

    Signal component

---
+++ Signals/Emitters/SignalComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Kalkatos.UnityGame.Scriptable
+{
+    [CreateAssetMenu(fileName = "NewSignalComponent", menuName = "Signals/Signal (Component)", order = 2)]
+    public class SignalComponent : TypedSignal<Component>
+    {
+        public Component DefaultValue;
+
+        public override void Emit ()
+        {
+            EmitWithParam(DefaultValue);
+        }
+    }
+}
