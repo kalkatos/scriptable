b9b63dec1d797b34db5324d17e397f59cd70b0a1
Alex Kalkatos <alexcsts@gmail.com>
2024-09-16 18:13:09 -0300

    Signal scriptable object

---
+++ Signals/Emitters/SignalScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Kalkatos.UnityGame.Scriptable
+{
+    [CreateAssetMenu(fileName = "NewSignalScriptableObject", menuName = "Signals/Signal (ScriptableObject)", order = 7)]
+	public class SignalScriptableObject : TypedSignal<ScriptableObject>
+	{
+        public ScriptableObject DefaultValue;
+
+        public override void Emit ()
+        {
+            EmitWithParam(DefaultValue);
+        }
+    }
+}
