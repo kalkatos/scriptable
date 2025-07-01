bbe00bc4a0f13bf74be531f33d7f50a7dfd970d8
Alex Kalkatos <alexcsts@gmail.com>
2025-06-30 22:45:04 -0300

    TmpTextSignal binding for any type turned into string

--- Signals/Receivers/TmpTextSignalBinding.cs
+++ Signals/Receivers/TmpTextSignalBinding.cs
@@ -8,7 +8,7 @@ namespace Kalkatos.UnityGame.Scriptable
 {
 	public class TmpTextSignalBinding : MonoBehaviour
 	{
-		[SerializeField] private TypedSignal<string> signal;
+		[SerializeField] private SignalBase signal;
 #if ODIN_INSPECTOR
 		[HideIf(nameof(textComponentInputField))]
 #endif
@@ -28,19 +28,69 @@ namespace Kalkatos.UnityGame.Scriptable
 
 		private void OnEnable ()
 		{
-			signal.OnSignalEmittedWithParam.AddListener(UpdateText);
-			UpdateText(signal.Value);
+			switch (signal)
+			{
+				case TypedSignal<string> stringSignal:
+					stringSignal.OnEmittedWithParam += UpdateText;
+					break;
+				case TypedSignal<bool> boolSignal:
+					boolSignal.OnEmittedWithParam += UpdateText;
+					break;
+				case TypedSignal<float> floatSignal:
+					floatSignal.OnEmittedWithParam += UpdateText;
+					break;
+				case TypedSignal<int> intSignal:
+					intSignal.OnEmittedWithParam += UpdateText;
+					break;
+				default:
+					Logger.LogError($"Signal {signal.name} is from a type not implemented.");
+					return;
+			}
 		}
 
 		private void OnDisable ()
 		{
-			signal.OnSignalEmittedWithParam.RemoveListener(UpdateText);
+			switch (signal)
+			{
+				case TypedSignal<string> stringSignal:
+					stringSignal.OnEmittedWithParam -= UpdateText;
+					break;
+				case TypedSignal<bool> boolSignal:
+					boolSignal.OnEmittedWithParam -= UpdateText;
+					break;
+				case TypedSignal<float> floatSignal:
+					floatSignal.OnEmittedWithParam -= UpdateText;
+					break;
+				case TypedSignal<int> intSignal:
+					intSignal.OnEmittedWithParam -= UpdateText;
+					break;
+				default:
+					return;
+			}
 		}
 
 		private void UpdateText (string text)
 		{
 			textComponent?.SetText(text);
 			textComponentInputField?.SetTextWithoutNotify(text);
+		}
+
+		private void UpdateText (int number)
+		{
+			textComponent?.SetText(number.ToString());
+			textComponentInputField?.SetTextWithoutNotify(number.ToString());
+		}
+
+		private void UpdateText (float number)
+		{
+			textComponent?.SetText(number.ToString());
+			textComponentInputField?.SetTextWithoutNotify(number.ToString());
+		}
+
+		private void UpdateText (bool b)
+		{
+			textComponent?.SetText(b.ToString());
+			textComponentInputField?.SetTextWithoutNotify(b.ToString());
 		}
 	}
 }
